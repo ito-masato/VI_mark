a11c39f4dc4814ca7db282ea67884ac669af0ca0
ito-masato <ito-masato@plazacreate.co.jp>
2016-05-27 19:22:43 +0900

    色塗り

---
+++ VI_mark/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace VI_mark
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_selectimg = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.btn_selectcolor1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_selectcolor2 = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pic_select1 = new System.Windows.Forms.PictureBox();
+            this.pic_select2 = new System.Windows.Forms.PictureBox();
+            this.btn_draw = new System.Windows.Forms.Button();
+            this.btn_paint = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_select1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_select2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_selectimg
+            // 
+            this.btn_selectimg.Location = new System.Drawing.Point(12, 23);
+            this.btn_selectimg.Name = "btn_selectimg";
+            this.btn_selectimg.Size = new System.Drawing.Size(62, 21);
+            this.btn_selectimg.TabIndex = 1;
+            this.btn_selectimg.Text = "画像選択";
+            this.btn_selectimg.UseVisualStyleBackColor = true;
+            this.btn_selectimg.Click += new System.EventHandler(this.btn_selectimg_Click);
+            // 
+            // btn_selectcolor1
+            // 
+            this.btn_selectcolor1.Location = new System.Drawing.Point(12, 50);
+            this.btn_selectcolor1.Name = "btn_selectcolor1";
+            this.btn_selectcolor1.Size = new System.Drawing.Size(62, 21);
+            this.btn_selectcolor1.TabIndex = 2;
+            this.btn_selectcolor1.Text = "色１選択";
+            this.btn_selectcolor1.UseVisualStyleBackColor = true;
+            this.btn_selectcolor1.Click += new System.EventHandler(this.btn_selectcolor1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.pictureBox1.Location = new System.Drawing.Point(80, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(529, 331);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Resize += new System.EventHandler(this.pictureBox1_Resize);
+            // 
+            // btn_selectcolor2
+            // 
+            this.btn_selectcolor2.Location = new System.Drawing.Point(12, 109);
+            this.btn_selectcolor2.Name = "btn_selectcolor2";
+            this.btn_selectcolor2.Size = new System.Drawing.Size(62, 21);
+            this.btn_selectcolor2.TabIndex = 4;
+            this.btn_selectcolor2.Text = "色２選択";
+            this.btn_selectcolor2.UseVisualStyleBackColor = true;
+            this.btn_selectcolor2.Click += new System.EventHandler(this.btn_selectcolor2_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_save.Location = new System.Drawing.Point(12, 333);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(62, 21);
+            this.btn_save.TabIndex = 5;
+            this.btn_save.Text = "保存";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // pic_select1
+            // 
+            this.pic_select1.BackColor = System.Drawing.Color.White;
+            this.pic_select1.Location = new System.Drawing.Point(12, 77);
+            this.pic_select1.Name = "pic_select1";
+            this.pic_select1.Size = new System.Drawing.Size(62, 23);
+            this.pic_select1.TabIndex = 6;
+            this.pic_select1.TabStop = false;
+            // 
+            // pic_select2
+            // 
+            this.pic_select2.BackColor = System.Drawing.Color.Blue;
+            this.pic_select2.Location = new System.Drawing.Point(12, 136);
+            this.pic_select2.Name = "pic_select2";
+            this.pic_select2.Size = new System.Drawing.Size(62, 23);
+            this.pic_select2.TabIndex = 7;
+            this.pic_select2.TabStop = false;
+            // 
+            // btn_draw
+            // 
+            this.btn_draw.Location = new System.Drawing.Point(12, 306);
+            this.btn_draw.Name = "btn_draw";
+            this.btn_draw.Size = new System.Drawing.Size(62, 21);
+            this.btn_draw.TabIndex = 8;
+            this.btn_draw.Text = "描画";
+            this.btn_draw.UseVisualStyleBackColor = true;
+            this.btn_draw.Visible = false;
+            this.btn_draw.Click += new System.EventHandler(this.btn_draw_Click);
+            // 
+            // btn_paint
+            // 
+            this.btn_paint.Location = new System.Drawing.Point(12, 165);
+            this.btn_paint.Name = "btn_paint";
+            this.btn_paint.Size = new System.Drawing.Size(62, 21);
+            this.btn_paint.TabIndex = 9;
+            this.btn_paint.Text = "塗る";
+            this.btn_paint.UseVisualStyleBackColor = true;
+            this.btn_paint.Click += new System.EventHandler(this.btn_paint_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 366);
+            this.Controls.Add(this.btn_paint);
+            this.Controls.Add(this.btn_draw);
+            this.Controls.Add(this.pic_select2);
+            this.Controls.Add(this.pic_select1);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_selectcolor2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_selectcolor1);
+            this.Controls.Add(this.btn_selectimg);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(500, 300);
+            this.Name = "Form1";
+            this.Text = "VI マーク作成モックアップ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_select1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_select2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_selectimg;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button btn_selectcolor1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_selectcolor2;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.PictureBox pic_select1;
+        private System.Windows.Forms.PictureBox pic_select2;
+        private System.Windows.Forms.Button btn_draw;
+        private System.Windows.Forms.Button btn_paint;
+
+        private System.Drawing.Imaging.ColorMap[] cmr = 
+                {
+                    new System.Drawing.Imaging.ColorMap(),
+                    new System.Drawing.Imaging.ColorMap()
+                };
+    }
+}
+
---
+++ VI_mark/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VI_mark
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 画像選択ダイアログ表示
+        /// </summary>
+        private void btn_selectimg_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialogクラスのインスタンスを作成
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //はじめのファイル名を指定する
+            ofd.FileName = null;
+            //はじめに表示されるフォルダを指定する
+            ofd.InitialDirectory = @".";
+            //[ファイルの種類]に表示される選択肢を指定する
+            ofd.Filter = "画像ファイル(*.png)|*.png";
+            //[ファイルの種類]ではじめに「画像ファイル」が選択されているようにする
+            ofd.FilterIndex = 1;
+            //タイトルを設定する
+            ofd.Title = "画像ファイルを選択してください";
+            //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+            ofd.RestoreDirectory = true;
+            //存在しないファイルの名前が指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckFileExists = true;
+            //存在しないパスが指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckPathExists = true;
+
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                // OKボタンがクリックされたとき
+                // 画像パスを格納する
+                //表示する画像を設定する
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox1.ImageLocation = ofd.FileName;
+
+                // 色情報初期化
+                // default Color1:SkyBlue(135,206,235)
+                // default Color2:PowderBlue(176,224,230)
+                cmr[0].OldColor = global::VI_mark.Properties.Settings.Default.Color1;
+                cmr[1].OldColor = global::VI_mark.Properties.Settings.Default.Color2;
+                
+            }
+
+            //コントロールを再描画する。
+            pictureBox1.Invalidate();
+        }
+
+        /// <summary>
+        /// 色選択1
+        /// </summary>
+        private void btn_selectcolor1_Click(object sender, EventArgs e)
+        {
+            //はじめに選択されている色を設定
+            colorDialog1.Color = pic_select1.BackColor;
+            //色の作成部分を表示可能にする
+            //デフォルトがTrueのため必要はない
+            colorDialog1.AllowFullOpen = true;
+            //純色だけに制限しない
+            //デフォルトがFalseのため必要はない
+            colorDialog1.SolidColorOnly = false;
+            //[作成した色]に指定した色（RGB値）を表示する
+            colorDialog1.CustomColors = new int[] {
+                0x33, 0x66, 0x99, 0xCC, 0x3300, 0x3333,
+                0x3366, 0x3399, 0x33CC, 0x6600, 0x6633,
+                0x6666, 0x6699, 0x66CC, 0x9900, 0x9933
+            };
+
+            //ダイアログを表示する
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //選択された色の取得
+                pic_select1.BackColor = colorDialog1.Color;
+
+            }
+        }
+
+        /// <summary>
+        /// 色選択2
+        /// </summary>
+        private void btn_selectcolor2_Click(object sender, EventArgs e)
+        {
+            //はじめに選択されている色を設定
+            colorDialog1.Color = pic_select2.BackColor;
+            //色の作成部分を表示可能にする
+            //デフォルトがTrueのため必要はない
+            colorDialog1.AllowFullOpen = true;
+            //純色だけに制限しない
+            //デフォルトがFalseのため必要はない
+            colorDialog1.SolidColorOnly = false;
+            //[作成した色]に指定した色（RGB値）を表示する
+            colorDialog1.CustomColors = new int[] {
+                0x33, 0x66, 0x99, 0xCC, 0x3300, 0x3333,
+                0x3366, 0x3399, 0x33CC, 0x6600, 0x6633,
+                0x6666, 0x6699, 0x66CC, 0x9900, 0x9933
+            };
+
+            //ダイアログを表示する
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //選択された色の取得
+                pic_select2.BackColor = colorDialog1.Color;
+
+            }
+        }
+
+        /// <summary>
+        /// PictureBoxリサイズイベント
+        /// </summary>
+        private void pictureBox1_Resize(object sender, EventArgs e)
+        {
+            // 再描画処理
+            ((Control)sender).Invalidate();
+        }
+
+        /// <summary>
+        /// 画像を保存
+        /// </summary>
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            // フィルターの設定
+            saveFileDialog1.Filter = "PNG|*.png";
+
+            // ファイル保存ダイアログを表示
+            saveFileDialog1.ShowDialog();
+        }
+
+        /// <summary>
+        ///  [保存]ボタンクリック時の処理
+        /// </summary>
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            string extension = System.IO.Path.GetExtension(saveFileDialog1.FileName);
+
+            try
+            {
+                switch (extension.ToUpper())
+                {
+                    case ".GIF":
+                        // PictureBoxのイメージをGIF形式で保存する
+                        pictureBox1.Image.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Gif);
+                        break;
+                    case ".JPG":
+                        // PictureBoxのイメージをJPEG形式で保存する
+                        pictureBox1.Image.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+                    case ".PNG":
+                        // PictureBoxのイメージをGIF形式で保存する
+                        pictureBox1.Image.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                        break;
+                }
+                MessageBox.Show("保存しました","保存");
+            }
+            catch (ArgumentNullException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+            }
+
+
+        }
+
+        private void btn_draw_Click(object sender, EventArgs e)
+        {
+            //描画先とするImageオブジェクトを作成する
+            Bitmap canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            //ImageオブジェクトのGraphicsオブジェクトを作成する
+            Graphics g = Graphics.FromImage(canvas);
+
+            Region rgn;
+            Rectangle rect1, rect2;
+
+            //Regionで使用する2つの領域
+            rect1 = new Rectangle(20, 20, 200, 200);
+            rect2 = new Rectangle(220, 20, 200, 200);
+
+            //rect1を青で塗る
+            g.FillRectangle(Brushes.SkyBlue, rect1);
+            //rect2を緑で塗る
+            g.FillRectangle(Brushes.PowderBlue, rect2);
+            //rect1でRegionを作成
+            rgn = new Region(rect1);
+            //Union(和集合)によりrect2を追加
+            rgn.Union(rect2);
+            //出来上がったRegionを黒で描画
+            //g.FillRegion(Brushes.Black, rgn);
+
+
+            //リソースを解放する
+            g.Dispose();
+
+            //pictureBox1に表示する
+            pictureBox1.Image = canvas;
+        }
+
+        /// <summary>
+        /// 指定された色を塗る
+        /// </summary>
+        private void btn_paint_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(pic_select1.BackColor == pic_select2.BackColor){
+                    DialogResult result = MessageBox.Show(
+                        "１色で塗り潰されます。" + Environment.NewLine + 
+                        "２色で塗り分ける場合は、画像選択からテンプレートを選び直してください。",
+                        "お知らせ",
+                        MessageBoxButtons.OKCancel,MessageBoxIcon.Exclamation );
+
+                    if (result == DialogResult.Cancel) { return; }
+                }
+
+                //描画先とするImageオブジェクトを作成する
+                Bitmap canvas = new Bitmap(pictureBox1.Image.Width, pictureBox1.Image.Height);
+                //Bitmap canvas = new Bitmap(global::VI_mark.Properties.Settings.Default.Width, 
+                //    global::VI_mark.Properties.Settings.Default.Height);
+                //ImageオブジェクトのGraphicsオブジェクトを作成する
+                Graphics g = Graphics.FromImage(canvas);
+
+                //ColorMapオブジェクトの配列（カラーリマップテーブル）を作成
+                System.Drawing.Imaging.ColorMap[] cms =
+                    new System.Drawing.Imaging.ColorMap[] 
+                {
+                    new System.Drawing.Imaging.ColorMap(),
+                    new System.Drawing.Imaging.ColorMap()
+                };
+
+                //SkyBlueを色１に変換する
+                cms[0].OldColor = cmr[0].OldColor;
+                cms[0].NewColor = pic_select1.BackColor;
+                //PowderBlueを色２に変換する
+                cms[1].OldColor = cmr[1].OldColor;
+                cms[1].NewColor = pic_select2.BackColor;
+                //色情報を保存
+                cmr[0].OldColor = pic_select1.BackColor;
+                cmr[1].OldColor = pic_select2.BackColor;
+
+                //ImageAttributesオブジェクトの作成
+                System.Drawing.Imaging.ImageAttributes ia =
+                    new System.Drawing.Imaging.ImageAttributes();
+                //ColorMapを設定
+                ia.SetRemapTable(cms);
+
+                //色を変換して画像を描画
+                g.DrawImage(pictureBox1.Image,
+                    new Rectangle(0, 0, pictureBox1.Image.Width, pictureBox1.Image.Height),
+                    0, 0, pictureBox1.Image.Width, pictureBox1.Image.Height,
+                    GraphicsUnit.Pixel,
+                    ia);
+
+                //Graphicsオブジェクトのリソースを解放する
+                g.Dispose();
+
+                // pictureBox1に表示する
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox1.Image = canvas;
+                
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"エラー");
+            }
+        }
+
+
+
+
+
+    }
+}
