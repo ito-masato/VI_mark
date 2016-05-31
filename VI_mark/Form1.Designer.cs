0c3d1a7f4ef002900b95f457a6cafed9d97f6ea5
ito-masato <ito-masato@plazacreate.co.jp>
2016-05-31 09:22:18 +0900

    文字配置(センタリング)

--- VI_mark/Form1.Designer.cs
+++ VI_mark/Form1.Designer.cs
@@ -38,8 +38,11 @@
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.pic_select1 = new System.Windows.Forms.PictureBox();
             this.pic_select2 = new System.Windows.Forms.PictureBox();
-            this.btn_draw = new System.Windows.Forms.Button();
             this.btn_paint = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.btn_fontset = new System.Windows.Forms.Button();
+            this.btn_txtdel = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pic_select1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pic_select2)).BeginInit();
@@ -71,9 +74,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.pictureBox1.Location = new System.Drawing.Point(80, 23);
+            this.pictureBox1.Location = new System.Drawing.Point(89, 23);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(529, 331);
+            this.pictureBox1.Size = new System.Drawing.Size(588, 351);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
@@ -92,7 +95,7 @@
             // btn_save
             // 
             this.btn_save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.btn_save.Location = new System.Drawing.Point(12, 333);
+            this.btn_save.Location = new System.Drawing.Point(12, 353);
             this.btn_save.Name = "btn_save";
             this.btn_save.Size = new System.Drawing.Size(62, 21);
             this.btn_save.TabIndex = 5;
@@ -122,17 +125,6 @@
             this.pic_select2.TabIndex = 7;
             this.pic_select2.TabStop = false;
             // 
-            // btn_draw
-            // 
-            this.btn_draw.Location = new System.Drawing.Point(12, 306);
-            this.btn_draw.Name = "btn_draw";
-            this.btn_draw.Size = new System.Drawing.Size(62, 21);
-            this.btn_draw.TabIndex = 8;
-            this.btn_draw.Text = "描画";
-            this.btn_draw.UseVisualStyleBackColor = true;
-            this.btn_draw.Visible = false;
-            this.btn_draw.Click += new System.EventHandler(this.btn_draw_Click);
-            // 
             // btn_paint
             // 
             this.btn_paint.Location = new System.Drawing.Point(12, 165);
@@ -143,13 +135,48 @@
             this.btn_paint.UseVisualStyleBackColor = true;
             this.btn_paint.Click += new System.EventHandler(this.btn_paint_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 271);
+            this.textBox1.MaxLength = 10;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(62, 19);
+            this.textBox1.TabIndex = 14;
+            // 
+            // fontDialog1
+            // 
+            this.fontDialog1.Color = System.Drawing.Color.White;
+            this.fontDialog1.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            // 
+            // btn_fontset
+            // 
+            this.btn_fontset.Location = new System.Drawing.Point(12, 296);
+            this.btn_fontset.Name = "btn_fontset";
+            this.btn_fontset.Size = new System.Drawing.Size(62, 21);
+            this.btn_fontset.TabIndex = 16;
+            this.btn_fontset.Text = "文字表示";
+            this.btn_fontset.UseVisualStyleBackColor = true;
+            this.btn_fontset.Click += new System.EventHandler(this.btn_fontset_Click);
+            // 
+            // btn_txtdel
+            // 
+            this.btn_txtdel.Location = new System.Drawing.Point(12, 323);
+            this.btn_txtdel.Name = "btn_txtdel";
+            this.btn_txtdel.Size = new System.Drawing.Size(62, 21);
+            this.btn_txtdel.TabIndex = 17;
+            this.btn_txtdel.Text = "消す";
+            this.btn_txtdel.UseVisualStyleBackColor = true;
+            this.btn_txtdel.Click += new System.EventHandler(this.btn_txtdel_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(621, 366);
+            this.ClientSize = new System.Drawing.Size(689, 386);
+            this.Controls.Add(this.btn_txtdel);
+            this.Controls.Add(this.btn_fontset);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btn_paint);
-            this.Controls.Add(this.btn_draw);
             this.Controls.Add(this.pic_select2);
             this.Controls.Add(this.pic_select1);
             this.Controls.Add(this.btn_save);
@@ -161,10 +188,12 @@
             this.MinimumSize = new System.Drawing.Size(500, 300);
             this.Name = "Form1";
             this.Text = "VI マーク作成モックアップ";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pic_select1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pic_select2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -179,7 +208,6 @@
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.PictureBox pic_select1;
         private System.Windows.Forms.PictureBox pic_select2;
-        private System.Windows.Forms.Button btn_draw;
         private System.Windows.Forms.Button btn_paint;
 
         private System.Drawing.Imaging.ColorMap[] cmr = 
@@ -187,6 +215,12 @@
                     new System.Drawing.Imaging.ColorMap(),
                     new System.Drawing.Imaging.ColorMap()
                 };
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Button btn_fontset;
+
+        private System.Drawing.Bitmap canvas_bk = null;
+        private System.Windows.Forms.Button btn_txtdel;
     }
 }
 
--- VI_mark/Form1.cs
+++ VI_mark/Form1.cs
@@ -54,6 +54,10 @@ namespace VI_mark
                 pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
                 pictureBox1.ImageLocation = ofd.FileName;
 
+                // 画像を背景に指定する
+                //pictureBox1.BackgroundImage = Image.FromFile(ofd.FileName);
+                //pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+                    
                 // 色情報初期化
                 // default Color1:SkyBlue(135,206,235)
                 // default Color2:PowderBlue(176,224,230)
@@ -167,6 +171,7 @@ namespace VI_mark
                     case ".PNG":
                         // PictureBoxのイメージをGIF形式で保存する
                         pictureBox1.Image.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                        //pictureBox1.BackgroundImage.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Png);
                         break;
                 }
                 MessageBox.Show("保存しました","保存");
@@ -182,38 +187,7 @@ namespace VI_mark
 
         }
 
-        private void btn_draw_Click(object sender, EventArgs e)
-        {
-            //描画先とするImageオブジェクトを作成する
-            Bitmap canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
-            //ImageオブジェクトのGraphicsオブジェクトを作成する
-            Graphics g = Graphics.FromImage(canvas);
 
-            Region rgn;
-            Rectangle rect1, rect2;
-
-            //Regionで使用する2つの領域
-            rect1 = new Rectangle(20, 20, 200, 200);
-            rect2 = new Rectangle(220, 20, 200, 200);
-
-            //rect1を青で塗る
-            g.FillRectangle(Brushes.SkyBlue, rect1);
-            //rect2を緑で塗る
-            g.FillRectangle(Brushes.PowderBlue, rect2);
-            //rect1でRegionを作成
-            rgn = new Region(rect1);
-            //Union(和集合)によりrect2を追加
-            rgn.Union(rect2);
-            //出来上がったRegionを黒で描画
-            //g.FillRegion(Brushes.Black, rgn);
-
-
-            //リソースを解放する
-            g.Dispose();
-
-            //pictureBox1に表示する
-            pictureBox1.Image = canvas;
-        }
 
         /// <summary>
         /// 指定された色を塗る
@@ -285,6 +259,159 @@ namespace VI_mark
             }
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //InstalledFontCollectionオブジェクトの取得
+            //System.Drawing.Text.InstalledFontCollection ifc =
+            //    new System.Drawing.Text.InstalledFontCollection();
+            //インストールされているすべてのフォントファミリアを取得
+            //FontFamily[] ffs = ifc.Families;
+            /*FontFamily[] ffs = FontFamily.Families;
+
+            foreach (FontFamily ff in ffs)
+            {
+                //スタイルにRegularが使用できるフォントのみを表示
+                if (ff.IsStyleAvailable(FontStyle.Regular))
+                {
+                    //Fontオブジェクトを作成
+                    Font fnt = new Font(ff, 8);
+                    //フォントをコンボボックスに追加
+                    cbb_font.Items.Add(fnt.Name);
+                    cbb_font.SelectedItem = "ＭＳ ゴシック";
+                    //リソースを解放する
+                    fnt.Dispose();
+                }
+            }
+
+            cbb_fontsize.SelectedItem = "14";*/
+
+        }
+
+        private void btn_txt_Click(object sender, EventArgs e)
+        {
+            //描画先とするImageオブジェクトを作成する
+            Bitmap canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            //ImageオブジェクトのGraphicsオブジェクトを作成する
+            Graphics g = Graphics.FromImage(canvas);
+
+            //フォントオブジェクトの作成
+            //Font fnt = new Font(cbb_font.SelectedItem.ToString(), float.Parse(cbb_fontsize.SelectedItem.ToString()));
+            //文字列を位置(0,0)、青色で表示
+            //g.DrawString(textBox1.Text, fnt, Brushes.White, 0, 0);
+
+            //リソースを解放する
+            //fnt.Dispose();
+            g.Dispose();
+
+            //PictureBox1に表示する
+            pictureBox1.Image = canvas;
+        }
+
+        /// <summary>
+        /// フォントダイアログ表示
+        /// </summary>
+        private void btn_fontset_Click(object sender, EventArgs e)
+        {
+            //FontDialogクラスのインスタンスを作成
+            FontDialog fd = new FontDialog();
+
+            //初期のフォントを設定
+            fd.Font = textBox1.Font;
+            //初期の色を設定
+            fd.Color = textBox1.ForeColor;
+            //ユーザーが選択できるポイントサイズの最大値を設定する
+            fd.MaxSize = 500;
+            fd.MinSize = 18;
+            //存在しないフォントやスタイルをユーザーが選択すると
+            //エラーメッセージを表示する
+            fd.FontMustExist = true;
+            //横書きフォントだけを表示する
+            fd.AllowVerticalFonts = false;
+            //色を選択できるようにする
+            fd.ShowColor = true;
+            //取り消し線、下線、テキストの色などのオプションを指定可能にする
+            //デフォルトがTrueのため必要はない
+            fd.ShowEffects = true;
+            //固定ピッチフォント以外も表示する
+            //デフォルトがFalseのため必要はない
+            fd.FixedPitchOnly = false;
+            //ベクタ フォントを選択できるようにする
+            //デフォルトがTrueのため必要はない
+            fd.AllowVectorFonts = true;
+
+            //ダイアログを表示する
+            if (fd.ShowDialog() != DialogResult.Cancel)
+            {
+
+                try
+                {
+                    //描画元のImageオブジェクトバックアップ
+                    canvas_bk = new Bitmap(pictureBox1.Image);
+                    //描画先とするImageオブジェクトを作成する
+                    Bitmap canvas = new Bitmap(pictureBox1.Image);
+                    //ImageオブジェクトのGraphicsオブジェクトを作成する
+                    Graphics g = Graphics.FromImage(canvas);
+
+                    //StringFormatオブジェクトの作成
+                    StringFormat sf = new StringFormat();
+
+                    //フォントオブジェクトの作成
+                    Font fnt = new Font(fd.Font.Name, fd.Font.Size);
+
+                    //幅の最大値をpitureBoxの幅(pixel)、文字列を描画するときの大きさを計測する
+                    //SizeF stringSize = g.MeasureString(textBox1.Text, fnt, pictureBox1.Width, sf);
+                    Size stringSize = TextRenderer.MeasureText(g, textBox1.Text, fnt,
+                         new Size(1000, 1000), TextFormatFlags.NoPadding);
+                    //取得した文字列の大きさを使って四角を描画する
+                    //g.DrawRectangle(Pens.Red, 
+                    //    (pictureBox1.Image.Width / 2) - (stringSize.Width / 2), 
+                    //    (pictureBox1.Image.Height / 2) - (stringSize.Height / 2), 
+                    //    stringSize.Width, stringSize.Height);
+
+                    //文字列を中心座標、選択した色で表示
+                    TextRenderer.DrawText(
+                        g, textBox1.Text, fnt,
+                        new Point((pictureBox1.Image.Width / 2) - (stringSize.Width / 2),
+                            (pictureBox1.Image.Height / 2) - (stringSize.Height / 2)),
+                        fd.Color, TextFormatFlags.NoPadding);
+
+                    //g.DrawString(textBox1.Text, fnt, Brushes.Red, 0, 0);
+
+                    //リソースを解放する
+                    fnt.Dispose();
+                    g.Dispose();
+
+                    //pictureBox1に表示する
+                    pictureBox1.Image = canvas;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "エラー");
+                }
+                
+            }
+        }
+
+        private void btn_txtdel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //ImageオブジェクトのGraphicsオブジェクトを作成する
+                Graphics g = Graphics.FromImage(canvas_bk);
+
+                //PictureBox1に表示する
+                pictureBox1.Image = canvas_bk;
+
+                //リソースを解放する
+                g.Dispose();
+                canvas_bk= null;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "エラー");
+            }
+        }
 
 
 
